c570962768b60c8733654193a294eb3a71d5d39a
AbhishekBalaramRaut <abhirait874@gmail.com>
2020-04-26 12:14:10 -0700

    Collection List /Set

--- src/com/abhishek/models/Triangle.java
+++ src/com/abhishek/models/Triangle.java
@@ -1,11 +1,13 @@
 package com.abhishek.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Triangle {
 
 	private String type;
-	private Point point1;
-	private Point point2;
-	private Point point3;
+	private List<Point> points;
+
 
 	public Triangle() {
 		
@@ -35,34 +37,17 @@ public class Triangle {
 		this.type = type;
 	}
 
-	public Point getPoint1() {
-		return point1;
+	
+	public List<Point> getPoints() {
+		return points;
 	}
-
-	public void setPoint1(Point point1) {
-		this.point1 = point1;
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
-
-	public Point getPoint2() {
-		return point2;
-	}
-
-	public void setPoint2(Point point2) {
-		this.point2 = point2;
-	}
-
-	public Point getPoint3() {
-		return point3;
-	}
-
-	public void setPoint3(Point point3) {
-		this.point3 = point3;
-	}
-
 	public void draw() {
 		System.out.println(type+" Triangle drawn ");
-		System.out.println(" Point 1 ("+point1.getPointX()+","+point1.getPointY()+")");
-		System.out.println(" Point 2 ("+point2.getPointX()+","+point2.getPointY()+")");
-		System.out.println(" Point 3 ("+point3.getPointX()+","+point3.getPointY()+")");
+		for(Point p: points) {
+			System.out.println(" Point : ("+p.getPointX()+","+p.getPointY()+")");		
+		}
 	}
 }
