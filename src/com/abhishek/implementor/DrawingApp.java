2f72b79d96227cc9d007eb631ad5d51d6a7cb8b8
AbhishekBalaramRaut <abhirait874@gmail.com>
2020-04-26 00:39:48 -0700

    Setter injection

--- src/com/abhishek/implementor/DrawingApp.java
+++ src/com/abhishek/implementor/DrawingApp.java
@@ -2,6 +2,8 @@ package com.abhishek.implementor;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 import com.abhishek.models.Triangle;
@@ -10,10 +12,10 @@ public class DrawingApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		BeanFactory beanF = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle t = (Triangle) beanF.getBean("triangle");
-		t.draw();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		//BeanFactory beanF = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle t = (Triangle) context.getBean("triangle");
+		System.out.println(t.getType());
 	}
 
 }
--- src/com/abhishek/models/Triangle.java
+++ src/com/abhishek/models/Triangle.java
@@ -2,6 +2,19 @@ package com.abhishek.models;
 
 public class Triangle {
 
+	private String type;
+
+	
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
 	public void draw() {
 		System.out.println("Triangle drawn");
 	}
