d6b3b0f198c13faafd9b4f703b21fccaf25b1167
AbhishekBalaramRaut <abhirait874@gmail.com>
2020-04-26 12:28:54 -0700

    Singleton vs prototype scopes

--- src/com/abhishek/models/Triangle.java
+++ src/com/abhishek/models/Triangle.java
@@ -2,44 +2,38 @@ package com.abhishek.models;
 
 public class Triangle {
 
-	private String type;
+
 	private Point point1;
 	private Point point2;
 	private Point point3;
 
 	public Triangle() {
-		
+		System.out.println("cons no arg");
 	}
 	public Triangle(String type) {
 		System.out.println("cons 1");
-		this.type = type;
+		
 	}
 
 	public Triangle(String type, int height) {
-		System.out.println("cons 3");
-		this.type = type;
+		System.out.println("cons 2");
+		
 
 	}
 
 	public Triangle(int height, String type) {
-
-		this.type = type;
-
+		System.out.println("cons 3");
+		
+		
 	}
 
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
 
 	public Point getPoint1() {
 		return point1;
 	}
 
 	public void setPoint1(Point point1) {
+		System.out.println("point 1set");
 		this.point1 = point1;
 	}
 
@@ -60,7 +54,7 @@ public class Triangle {
 	}
 
 	public void draw() {
-		System.out.println(type+" Triangle drawn ");
+		System.out.println(" Triangle drawn ");
 		System.out.println(" Point 1 ("+point1.getPointX()+","+point1.getPointY()+")");
 		System.out.println(" Point 2 ("+point2.getPointX()+","+point2.getPointY()+")");
 		System.out.println(" Point 3 ("+point3.getPointX()+","+point3.getPointY()+")");
