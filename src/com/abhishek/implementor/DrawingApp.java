c535847ca301008ac4e781d234bc3369cc564b54
AbhishekBalaramRaut <abhirait874@gmail.com>
2020-04-28 03:30:32 -0700

    Lifecycle Hooks

--- src/com/abhishek/implementor/DrawingApp.java
+++ src/com/abhishek/implementor/DrawingApp.java
@@ -3,6 +3,7 @@ package com.abhishek.implementor;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
@@ -12,7 +13,8 @@ public class DrawingApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		//BeanFactory beanF = new XmlBeanFactory(new FileSystemResource("spring.xml"));
 		Triangle t = (Triangle) context.getBean("triangle");
 		t.draw();
--- src/com/abhishek/models/Triangle.java
+++ src/com/abhishek/models/Triangle.java
@@ -3,11 +3,14 @@ package com.abhishek.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class Triangle {
+public class Triangle implements InitializingBean, DisposableBean, ApplicationContextAware, BeanNameAware{
 
 
 	private List<Point> points = new ArrayList<>(); 
@@ -29,6 +32,7 @@ public class Triangle {
 	}
 
 	public void setPoints(List<Point> points) {
+		System.out.println("Calling setter");
 		this.points = points;
 	}
 
@@ -37,6 +41,35 @@ public class Triangle {
 		for(Point point1: points) {
 			System.out.println(" Point 1 ("+point1.getPointX()+","+point1.getPointY()+")");
 		}
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Initializing");
+		
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("Destroy");
+	}
+	public void myInit() throws Exception {
+		System.out.println("myinit");
+	}
+	
+	public void myDestroy() throws Exception {
+		System.out.println("myDestroy");
+	}
+
+	@Override
+	public void setBeanName(String arg0) {
+		System.out.println("Bean is "+arg0);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+		System.out.println("context is "+arg0);
 		
 	}
 }
