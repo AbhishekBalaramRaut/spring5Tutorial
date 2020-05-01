4a3ba209e27011bd146ae6fc7470a4ff94715a4c
AbhishekBalaramRaut <abhirait874@gmail.com>
2020-05-01 12:50:50 -0700

    @Pointcut and wildcards

--- src/com/abhishek/aop/LoggingAspect.java
+++ src/com/abhishek/aop/LoggingAspect.java
@@ -1,15 +1,26 @@
 package com.abhishek.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
 import com.abhishek.models.Point;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public com.abhishek.models.Point getCenter())")
-	 public void LoggingAdvice() {
+	@Before("MyPointcut()")
+	 public void LoggingAdviceBefore() {
 		 System.out.println("Before logging advice");
 	 }
-	
+	 @After("MyPointcut()")
+	 public void LoggingAdviceAfter() {
+		 System.out.println("After logging advice");
+	 }
+	 
+	 @Pointcut("execution(*  get*())")
+	 public void MyPointcut() {
+		
+	 }
 }
